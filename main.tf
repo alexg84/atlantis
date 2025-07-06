1fad23326035620363d2031d5582edafd1afcdf7
Alex <alex@Alexs-MBP.lan>
2025-07-06 19:01:16 +0300

    atlantis_poc_result

--- main.tf
+++ main.tf
@@ -16,21 +16,9 @@ terraform {
   }
 }
 
-# Simple random resources for testing
-resource "random_string" "app_suffix" {
+# Simple random string for Atlantis POC testing
+resource "random_string" "atlantis_poc" {
   length  = 8
   special = false
   upper   = false
-}
-
-# Simple null resource for testing
-resource "null_resource" "simple_test" {
-  triggers = {
-    app_name = var.app_name
-    suffix   = random_string.app_suffix.result
-  }
-
-  provisioner "local-exec" {
-    command = "echo 'Testing Atlantis with ${var.app_name}-${random_string.app_suffix.result} in ${var.environment} environment'"
-  }
 }
